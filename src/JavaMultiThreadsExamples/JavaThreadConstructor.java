6bb765bd0a3251a53133817256370c0e20458f9f
GuruCharan02324 <gurucharan.p@excelr.com>
2026-04-21 20:06:51 +0530

    GCD 21-04-2026

---
+++ src/JavaMultiThreadsExamples/JavaThreadConstructor.java
@@ -0,0 +1,164 @@
+package JavaMultiThreadsExamples;
+
+// Thread Constructor With Task execution :
+class MyThreads1 implements Runnable{
+	public void run() {
+		System.out.println("Running Method :" + Thread.currentThread().getName());
+	}
+}
+
+public class JavaThreadConstructor {
+  public static void main(String[] args) {
+	  MyThreads1 mt = new MyThreads1();
+	  Thread t = new Thread(mt, "MyThread");
+	  t.start();
+	  
+	  //System.out.println("Thread Name : " + t.getName());
+  }
+}
+
+
+// Thread Naming : 
+class Test {
+	  public static void main(String[] args) {
+		  // Getting Current Thread name : 
+		  System.out.println(Thread.currentThread().getName());
+		  	  
+		  // changing current thread name 
+		 Thread.currentThread().setName("CustomThread");
+		 
+		 // Print updated thread name : 
+		 System.out.println(Thread.currentThread().getName());
+	  }
+}
+
+// Multiple Threading :
+
+class MyMulThread extends Thread {
+	public void run() {
+		System.out.println("Running : "+ Thread.currentThread().getName());
+	}
+}
+
+class TestMulThreads{
+	  public static void main(String[] args) {
+		  MyMulThread t1 = new MyMulThread();
+		  MyMulThread t2 = new MyMulThread();
+		  
+		  t1.setName("Worker-1");
+		  t2.setName("Worker-2");
+		  
+		  t1.start();
+		  t2.start();
+	  }
+}
+// Thread Priority : 
+
+class TestThreadPriority{
+	 public static void main(String[] args) {
+		 Thread t = new Thread(); 
+		 t.setPriority(8);
+		 System.out.println(t.getPriority());
+	 }
+}
+// Prevent Thread Execution : (Control flow of Thread Execution);
+// yield(); : give chance to other threads 
+// join(); : wait for another thread 
+// sleep(); : pause for some time 
+
+//1. yield()  : static method, no guarantee of pause, used rarely in real application. (I'm ready to pause, let other threads run)
+class MyThread5 extends Thread{
+	public void run() {
+		for(int i = 0; i<=3; i++) {
+			System.out.println("Child Thread");
+			Thread.yield();
+		}
+	}
+}
+
+class DemoMyThread5{
+	 public static void main(String[] args) {
+		 MyThread5 t5 = new MyThread5();
+		 t5.start();
+			for(int i = 0; i<=3; i++) {
+				System.out.println("Main Thread");
+			}
+	 }
+}
+
+//2. join() : You finish first, Then I continue (non -static method, Guarantees execution order , Throws Interrupted Exception)
+ 
+class MyThread6 extends Thread{
+	public void run() {
+			System.out.println("Child Thread");
+		}
+}
+
+class DemoMyThread6{
+	 public static void main(String[] args) throws Exception {
+		 MyThread6 t6 = new MyThread6();
+		 t6.start();
+		 t6.join();
+		 System.out.println("Main Thread");		
+	 }
+} 
+
+// 3. sleep(): pause me for sometimes  ( static method , time in mili-sec, always throws Interrupted exception, Guarantees delay)
+
+class Test1 {
+	 public static void main(String[] args) throws Exception {
+		 System.out.println("Start");
+		 Thread.sleep(4000);
+		 System.out.println("End");
+	 }
+}
+
+// Interrupting Thread : (Interrupt, Interrupted and isInterrupted)
+class MyThreads01 extends Thread{
+	public void run() {
+		try {
+			Thread.sleep(3000);
+		}catch(InterruptedException e) {
+			System.out.println(e.getMessage());
+		}
+	}
+}
+
+class Test01{
+	public static void main(String[] args) {
+		MyThreads01 t01 = new MyThreads01();
+		
+		t01.start();
+		t01.interrupt();
+	}
+}
+// Better example : 
+class MyThreads011 extends Thread{
+	public void run() {
+		for(int i =0; i<=3; i++) {
+			if(Thread.interrupted()) {
+				System.out.println("Stopped due to interrupt...");
+				break;
+			}
+			System.out.println("Working...");
+		}
+	}
+}
+
+class Test011{
+	public static void main(String[] args) {
+		MyThreads011 t011 = new MyThreads011();
+		t011.start();
+		t011.interrupt();
+	}
+}
+
+
+
+
+
+
+
+
+
+
--- src/JavaMultiThreadsExamples/JavaThreadIntro.java
+++ src/JavaMultiThreadsExamples/JavaThreadIntro.java
@@ -19,8 +19,9 @@ class MyThread extends Thread{
 	}
 }
 
-// Thread Scheduler : 
-
+// Thread Scheduler => It's part of JVM and if multiple threads are waiting to execute then which thread will execute 1st is decided by "[Thread Scheduler]". 
+// Importance of start() ( Use MultiThreading or Thread concept)
+// Not Overriding run();
 class TestThread extends Thread {
 	public void run() {
 		for(int i = 0; i<=5;i++) {
@@ -32,12 +33,93 @@ class TestThread extends Thread {
 class TestThreadDemo{
 	public static void main(String[] args) {
 		TestThread td = new TestThread();
-		td.run();
+		td.start();  // create new thread
+		td.run();  // Normal method calling
 		for(int i = 0; i<=5; i++) {
 			System.out.println("Main Thread");
 		}
 	}
 }
+//Overloading follow by Thread concept (Example)
+
+class MyThread1 extends Thread{
+	public void run() {
+		System.out.println("No arg run()");
+	}
+    public void run(int i) {
+		System.out.println("Integer arg run" + i);
+	}
+}
+class MainThread{
+	public static void main(String[] args) {
+		MyThread1 td1 = new MyThread1();
+		td1.start();
+		td1.run(12);
+	}
+}
+
+// Overriding Start() method in Thread : 
+class MyThread2 extends Thread{
+	public void start() {
+		System.out.println("Start Method");
+	}
+    public void run() {
+		System.out.println("Run Method");
+	}
+}
+class MainThread2{
+	public static void main(String[] args) {
+		MyThread2 td2 = new MyThread2();
+		td2.start();
+		System.out.println("Main Method");
+	}
+}
+
+// Thread Life Cycle :
+// 1. New Born Thread : Object creation 
+// 2. Runnable : calling of start() method
+// 3. Running : start()  assign(JVM) ====> run()
+// 4. Dead : run() completed 
+
+
+// Re-Starting of Thread : 
+class Mythread3 extends Thread{
+	public void run(){
+		System.out.println("Thraed is running...");
+	}
+}
+
+class MainClass{
+	public static void main(String[] args) {
+		Mythread3 td3 = new Mythread3();		
+		td3.start(); // first thread
+		
+		Mythread3 td4 = new Mythread3();
+		td4.start(); // Second Thread
+	}
+}
+
+class MyTest extends Thread{
+	public void run() {
+		display();  // Custom method
+	}
+	public void display() {
+		System.out.println("Display Code..");
+		execution();
+	}
+	public void execution(){
+		System.out.println("Execution of Code..");
+	}
+}
+
+class TestMainDemo{
+	public static void main(String[] args) {
+		MyTest mt = new MyTest();
+		mt.start();
+	}
+}
+
+//-------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 
 // By Implementing Runnable interface  : Support for Multiple Inheritance 
@@ -52,4 +134,26 @@ class MyRunnable implements Runnable{
 		t.start();
 	}
 	 
+}
+// basic Example for Runnable Interface case : 
+class MyThreadRunnable implements Runnable{
+	public void run() {
+		for(int i=0; i<=5; i++) {
+			System.out.println("Child Runnable Thread");
+		}
+	}
+}
+
+class MainRunnableClass {
+	public static void main(String[] args) {
+		
+		MyThreadRunnable r = new MyThreadRunnable();
+		Thread t = new Thread(r);  // Thread Constructor	
+		t.start();
+		
+		for(int i=0; i<=5; i++) {
+			System.out.println("Main Runnable Thread");
+		}
+	}
+	
 }
